6696564a54b299074f013eeb4c95315f334074c1
myaitestacct <myaitestacct@gmail.com>
2026-08-24 15:54:05 -0500

    Fix footer.php to ensure proper HTML closure

--- movie-catalog/src/views/layout/footer.php
+++ movie-catalog/src/views/layout/footer.php
@@ -1,3 +1,13 @@
+<?php
+// Use bundled JS if available (production), otherwise load ES modules individually (dev)
+$publicRoot = dirname(__DIR__, 3) . '/public';
+$bundleJs = $publicRoot . '/assets/dist/bundle.js';
+$hasBundleJs = is_file($bundleJs);
+?>
+<?php if ($hasBundleJs): ?>
+<script type="module" src="assets/dist/bundle.js"></script>
+<?php else: ?>
 <script type="module" src="assets/js/app.js"></script>
+<?php endif; ?>
 </body>
 </html>
